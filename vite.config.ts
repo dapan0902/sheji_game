95601cd1464be017c043d6f2e67e667fe46b1d6f
Mac <mac@wp-MacBook-Pro.local>
2025-11-24 00:02:26 +0800

    配置 GitHub Pages部署

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/sheji_game/', // GitHub Pages路径
   server: {
     host: '0.0.0.0',
     port: 3000,
